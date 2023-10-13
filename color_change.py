23586f5cd668212251470a468c31c4b249916988
Whitedragon12 <48678571+Whitedragon12@users.noreply.github.com>
2023-10-13 22:08:35 +0300

    add class color_change

---
+++ color_change.py
@@ -0,0 +1,29 @@
+class Colorizer:
+    colors = {
+        'reset': '\033[0m',
+        'black': '\033[30m',
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'magenta': '\033[35m',
+        'cyan': '\033[36m',
+        'white': '\033[37m'
+    }
+
+    def __init__(self, color):
+        self.color = color
+
+    def __enter__(self):
+        if self.color in Colorizer.colors:
+            print(Colorizer.colors[self.color], end='')
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        print(Colorizer.colors['reset'], end='')
+
+
+
+with Colorizer('red'):
+    print('printed in red')
+
+print('printed in default color')
